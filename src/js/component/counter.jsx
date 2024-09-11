13377ca94fabc2cc91559bfe955ae59ed05b91b6
Patricia-Berges <patry37.zgz@gmail.com>
2024-09-11 15:59:36 +0000

    Added style

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -2,19 +2,43 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "react"
 
 const Counter = ({timer}) => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   
-    useEffect(() => {
-      const counter = setInterval(() => {
-        setCount((prevCount) => (prevCount === 9 ? 0 : prevCount + 1));
-      }, timer);  
-      return () => clearInterval(counter);
-        }, [timer]);
-        
+  useEffect(() => {
+    const counter = setInterval(() => {
+      setCount((prevCount) => (prevCount === 9 ? 0 : prevCount + 1));
+    }, timer);  
+    return () => clearInterval(counter);
+      }, [timer]);
+   
     return(
-        <div>{count}</div>
-    )
+    <div style={secondsStyle}>
+      <span style={numberStyle} >{count}</span>
+    </div>
+  )
+}
+
+const secondsStyle = {
+  height: "100px",
+  width: "75px",
+  background: "rgb(20, 20, 20)",
+  textAlign: "center",
+  color: "white",
+  margin: "10px",
+  position: "relative",
+  borderRadius: "10px",
+  borderLeft: "solid 2px grey",
+  borderRight: "solid 2px grey"
+}
+
+const numberStyle = {
+  top: "50%",
+  transform: "translate(-50%, -50%)",
+  position: "absolute",
+  margin: "0px",
+  padding: "0px",
+  fontSize: "70px"
 }
 
 
-export {Counter}
+export {Counter, secondsStyle, numberStyle}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Counter } from "./counter";
-
-
+import { Counter, secondsStyle, numberStyle } from "./counter";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 //create your first component
-const Home = () => {
+const SimpleCounter = () => {
 	return (
-		<div>
+		<div className="d-flex bg-black justify-content-center m-3">
+			<div style={secondsStyle} >
+				<span style={numberStyle}><FontAwesomeIcon icon="fa-solid fa-clock-nine" /></span>
+			</div>
 			<Counter timer={100000000} />
 			<Counter timer={10000000} />
 			<Counter timer={1000000} />
@@ -17,4 +19,4 @@ const Home = () => {
 	);
 };
 
-export default Home;
+export default SimpleCounter;
